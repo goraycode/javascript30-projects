4b66d773e945c82ac327f0473ce21fada3fc0586
AYGJ <jofre1236@gmail.com>
2022-06-02 17:42:50 -0500

    Add audio

--- project1/js/app.js
+++ project1/js/app.js
@@ -2,4 +2,8 @@ window.addEventListener('keydown', keyboard);
 
 function keyboard(e) {
     console.log(e);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    audio.play();
 }
