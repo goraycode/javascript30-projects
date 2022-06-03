3ce537ff75a341d2f9a42b21324c26ccea964d13
AYGJ <jofre1236@gmail.com>
2022-06-03 12:45:37 -0500

    Add new function about click in the box

--- project1/js/app.js
+++ project1/js/app.js
@@ -3,13 +3,34 @@ window.addEventListener('keydown', keyboard);
 function keyboard(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const box = document.querySelector(`.box[data-key="${e.keyCode}"]`);
-    if (!audio) return;
+    playSoundandEffect(audio, box);
+}
 
+
+function clickBox() {
+    const boxes = document.querySelectorAll(`.box`);
+
+    boxes.forEach(box => {
+        box.addEventListener('click', (e) => {
+            console.log(e.target.attributes[0].value);
+            const valueSound = e.target.attributes[0].value
+            const audio = document.querySelector(`audio[data-key="${valueSound}"]`);
+            playSoundandEffect(audio, box);
+        })
+    });
+}
+
+
+function playSoundandEffect(audio, box) {
+    if (!audio) return;
     audio.currentTime = 0; //rewind to the start
     audio.play();
 
     box.classList.add('box__effect');
     setTimeout(() => {
         box.classList.remove('box__effect');
-    }, 1000);
+    }, 250);
 }
+
+
+clickBox();
