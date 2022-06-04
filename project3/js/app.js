7865ba98a151ccfd5bb5849b8d042e24237cd616
AYGJ <jofre1236@gmail.com>
2022-06-04 17:06:49 -0500

    Add new function for events

--- project3/js/app.js
+++ project3/js/app.js
@@ -1,7 +1,24 @@
 const img = document.querySelector('.img');
+const beach = document.querySelector('.beach');
+const titleJS = document.querySelector('.title__lyrics');
 
-const spacing = document.querySelector('#spacing');
 
-spacing.addEventListener('change', () => {
-    img.style.borderWidth = `${spacing.value}px`;
-})
+eventListeners();
+function eventListeners() {
+
+    const spacing = document.querySelector('#spacing');
+    spacing.addEventListener('change', () => {
+        img.style.borderWidth = `${spacing.value}px`;
+    });
+
+    const blur = document.querySelector('#blur');
+    blur.addEventListener('change', () => {
+        beach.style.filter = `blur(${blur.value}px)`;
+    })
+
+    const color = document.querySelector('#color');
+    color.addEventListener('change', () => {
+        img.style.borderColor = `${color.value}`;
+        titleJS.style.color = `${color.value}`;
+    })
+}
