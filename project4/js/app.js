c0913379722e3de63d5878b5be1709fa850f6235
AYGJ <jofre1236@gmail.com>
2022-06-05 22:52:27 -0500

    Add new exercise 3

--- project4/js/app.js
+++ project4/js/app.js
@@ -1,12 +1,20 @@
 const inventors = [
-    { 'first': 'Albert', 'Second': 'Einstein', 'year': 1580 },
-    { 'first': 'Galileo', 'Second': 'Galilei', 'year': 1350 },
-    { 'first': 'Isaac', 'Second': 'Newton', 'year': 1560 }
+    { 'first': 'Albert', 'last': 'Einstein', 'year': 1580 },
+    { 'first': 'Galileo', 'last': 'Galilei', 'year': 1350 },
+    { 'first': 'Isaac', 'last': 'Newton', 'year': 1560 }
 ]
 
 console.table(inventors);
 
-
+//filter
 //1. Filter the list of inventors for those who were born in the 1500's
 const fifteen = inventors.filter(inventor => inventor.year >= 1500 && inventor.year < 1600)
 console.table(fifteen);
+
+
+//map
+//2. Give us an array of the inventory first and last names
+const inventory = inventors.map(inventor => `${inventor.first}, ${inventor.last}`);
+console.table(inventory);
+
+
