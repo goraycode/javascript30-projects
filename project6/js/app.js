d6bc8e7f7e18a7e573002dedbc9cadec29cc9866
AYGJ <jofre1236@gmail.com>
2022-06-07 17:08:24 -0500

    Add db.json and filter foods

--- project6/js/app.js
+++ project6/js/app.js
@@ -6,6 +6,23 @@ function eventListener() {
 }
 
 function searchFood(e) {
-    console.log(e.target.value);
+    const foodSearch = e.target.value;
+
     //fetch here
+    const url = 'db.json';
+
+    fetch(url)
+        .then(blob => blob.json())
+        .then(result => filterFoods(result, foodSearch))
+}
+
+function filterFoods(result, foodSearch) {
+
+    const results = document.querySelector('.results');
+    if (!foodSearch.length) return;
+    const filterFood = result.filter(food => food.name.toLowerCase().includes(`${foodSearch}`));
+    
+    //scripting
+    
+    
 }
