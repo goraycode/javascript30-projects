2d493d7d9d9a19670a3a0d57262859934a3442e3
AYGJ <jofre1236@gmail.com>
2022-06-07 17:18:31 -0500

    Add new function cleanHTML

--- project6/js/app.js
+++ project6/js/app.js
@@ -19,10 +19,39 @@ function searchFood(e) {
 function filterFoods(result, foodSearch) {
 
     const results = document.querySelector('.results');
+
     if (!foodSearch.length) return;
+
+    cleanHTML();
+
     const filterFood = result.filter(food => food.name.toLowerCase().includes(`${foodSearch}`));
-    
-    //scripting
-    
-    
+    filterFood.forEach(food => {
+
+        //destructuring
+        const { name, price } = food;
+
+        //scripting
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        const cardTitle = document.createElement('h2');
+        cardTitle.className = 'card__title';
+        cardTitle.textContent = name;
+
+        const cardPrice = document.createElement('card__price');
+        cardPrice.className = 'card__price';
+        cardPrice.textContent = price;
+
+        card.appendChild(cardTitle);
+        card.appendChild(cardPrice);
+
+        results.appendChild(card);
+    });
+}
+
+function cleanHTML() {
+    const results = document.querySelector('.results');
+    while (results.firstChild) {
+        results.removeChild(results.firstChild);
+    }
 }
