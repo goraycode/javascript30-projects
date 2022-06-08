8144ae83fa85691a23be5f2ef84d37b3b424e10f
AYGJ <jofre1236@gmail.com>
2022-06-08 17:00:43 -0500

    Add new exercise with some

---
+++ project7/js/app.js
@@ -0,0 +1,23 @@
+const people = [
+    { name: 'West', year: 1988 },
+    { name: 'Kait', year: 1986 },
+    { name: 'Irv', year: 1970 },
+    { name: 'Lux', year: 2015 }
+];
+
+const comments = [
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen in my fav food ever', id: 123523 },
+    { text: 'Nice Nice Nice!', id: 542328 }
+];
+
+//Some and every checks
+//Array some //is at least one person 19?
+const nineteen = people.some(p => {
+    if (2022 - p.year === 19) return p;
+})
+
+console.log(nineteen);
+
