c24df9e56da997ebc8ff947ab7b734c4f119f24f
AYGJ <jofre1236@gmail.com>
2022-06-09 17:08:38 -0500

    Add types of console

---
+++ project9/js/app.js
@@ -0,0 +1,23 @@
+const dogs = [
+    { name: 'Snickers', age: 2 },
+    { name: 'Hugo', age: 8 }
+];
+
+//different console
+
+//regular
+console.log('hello');
+
+//interpolated
+//%s === second parameter
+console.log('Hello I am a %s string!', '🙂');
+
+//styled
+//%c it's important for give a styles
+console.log('%c Hola', 'font-size:2rem; color:red');
+
+//warning
+console.warn('Oh no!');
+
+//error
+console.error('Impossible');
