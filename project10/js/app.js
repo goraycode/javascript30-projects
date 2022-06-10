29b08916922d0c311c2ce377001a0dc75bc9e76a
AYGJ <jofre1236@gmail.com>
2022-06-10 16:52:13 -0500

    Add new code for resolve the problem

--- project10/js/app.js
+++ project10/js/app.js
@@ -1,13 +1,35 @@
 const boxes = document.querySelectorAll('input[type="checkbox"]');
 const ps = document.querySelectorAll('p');
 
-let counter = 0;
+let check = [], old = 0;
 boxes.forEach((box, index) => {
     box.addEventListener('change', (e) => {
+        check = [...check, index];
         const boxCheck = e.target.checked;
+
+
         if (boxCheck) {
-            
+            console.log(check);
             ps[index].classList.add('trash');
+
+            for (let i = check[0]; i < check[check.length - 1]; i++) {
+                ps[i].classList.add('trash');
+                boxes[i].checked = true;
+            }
+
+            if (check[0] > check[check.length - 1]) {
+                [initial, final] = [check[check.length - 1], check[0]];
+
+                for (let i = initial; i < final; i++) {
+                    console.log(i);
+                    ps[i].classList.add('trash');
+                    boxes[i].checked = true;
+                }
+            }
+
+
+
+
         } else {
             ps[index].classList.remove('trash');
         }
@@ -16,11 +38,3 @@ boxes.forEach((box, index) => {
 });
 
 
-function disabledAll() {
-    ps.forEach(p => {
-        p.classList.add('trash');
-    });
-    boxes.forEach(box => {
-        box.checked = true;
-    });
-}
