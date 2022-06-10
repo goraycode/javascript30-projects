8f1e3b2b72348466cda24eb64619cebb84a07890
AYGJ <jofre1236@gmail.com>
2022-06-10 17:08:30 -0500

    Add more styles for items

--- project10/js/app.js
+++ project10/js/app.js
@@ -1,5 +1,6 @@
 const boxes = document.querySelectorAll('input[type="checkbox"]');
 const ps = document.querySelectorAll('p');
+const items = document.querySelectorAll('.item');
 
 let check = [], old = 0;
 boxes.forEach((box, index) => {
@@ -12,26 +13,26 @@ boxes.forEach((box, index) => {
             console.log(check);
             ps[index].classList.add('trash');
 
-            for (let i = check[0]; i < check[check.length - 1]; i++) {
+            for (let i = check[0]; i <= check[check.length - 1]; i++) {
                 ps[i].classList.add('trash');
                 boxes[i].checked = true;
+                items[i].classList.add('checked');
             }
 
             if (check[0] > check[check.length - 1]) {
                 [initial, final] = [check[check.length - 1], check[0]];
 
-                for (let i = initial; i < final; i++) {
+                for (let i = initial; i <= final; i++) {
                     console.log(i);
                     ps[i].classList.add('trash');
                     boxes[i].checked = true;
+                    items[i].classList.add('checked');
                 }
             }
 
-
-
-
         } else {
             ps[index].classList.remove('trash');
+            items[i].classList.remove('checked');
         }
 
     })
