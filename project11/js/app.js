1e8f03399b275eeaa669ffe60ad02c1e099f3f4a
AYGJ <jofre1236@gmail.com>
2022-06-11 16:51:21 -0500

    Add video and new event

--- project11/js/app.js
+++ project11/js/app.js
@@ -1,2 +1,13 @@
 const playerVideo = document.querySelector('.viewer');
-console.log(playerVideo);
+const btnPlay = document.querySelector('.play');
+const volume = document.querySelector('#volume');
+
+let counter = 0;
+btnPlay.addEventListener('click', () => {
+    playerVideo.play();
+})
+
+volume.addEventListener('change', (e) => {
+    const valueVol = e.target.value;
+    playerVideo.volume = valueVol;
+})
