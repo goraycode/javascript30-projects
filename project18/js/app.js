832b6a36d0a7b43fab321de6f90944784f8838fa
AYGJ <jofre1236@gmail.com>
2022-06-17 17:53:14 -0500

    Finished project 18 v1.0.0

---
+++ project18/js/app.js
@@ -0,0 +1,21 @@
+const timeNodes = Array.from(document.querySelectorAll('[data-time]'));
+
+const seconds = timeNodes
+    .map(node => node.dataset.time)
+    .map(timeCode => {
+        const [min, sec] = timeCode.split(':').map(parseFloat)
+        return (min * 60) + sec;
+    })
+    .reduce((acc, cur) => acc + cur, 0);
+
+let secondsLeft = seconds;
+let hours = Math.floor(seconds / 3600);
+secondsLeft = secondsLeft % 3600;
+let min = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+console.log(hours, min, secondsLeft);
+
+
+
+
+
