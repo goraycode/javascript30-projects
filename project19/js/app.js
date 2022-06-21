26b75b04a9d375f904982749b0f20f5ae210f33d
AYGJ <jofre1236@gmail.com>
2022-06-20 19:14:25 -0500

    Add function greenScreen

--- project19/js/app.js
+++ project19/js/app.js
@@ -21,7 +21,21 @@ function paintToCanvas() {
     canvas.height = height;
 
     return setInterval(() => {
-        ctx.drawImage(video, 0, 0, width, height)
+        ctx.drawImage(video, 0, 0, width, height);
+        let pixels = ctx.getImageData(0, 0, width, height);
+
+        //mess with them
+        //pixels = redEffect(pixels);
+
+        /*     pixels = rgbSplit(pixels);
+    
+            ctx.globalAlpha = 1; */
+
+        pixels = greenScreen(pixels);
+
+        //put them back
+        ctx.putImageData(pixels, 0, 0);
+
     }, 16);
 
 
@@ -41,6 +55,58 @@ function takePhoto() {
     strip.insertBefore(link, strip.firstChild);
 }
 
+
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+
+        pixels.data[i + 0] = pixels.data[i + 0] + 50 //red
+        pixels.data[i + 1] = pixels.data[i + 1] - 50//green
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5//blue
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+
+        pixels.data[i - 150] = pixels.data[i + 0]; //red
+        pixels.data[i + 100] = pixels.data[i + 1];//green
+        pixels.data[i - 550] = pixels.data[i + 2];//blue
+    }
+    return pixels;
+}
+
+function greenScreen(pixels) {
+    const levels = {};
+
+    const inputs = document.querySelectorAll('.control input');
+
+    inputs.forEach(input => {
+        levels[input.name] = input.value;
+    });
+
+    for (let i = 0; i < pixels.data.length; i += 4) {
+
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+
+        if (red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red <= levels.rmax
+            && green <= levels.gmax
+            && blue <= levels.bmax) {
+
+            pixels.data[i + 3] = 0;
+        }
+
+
+    }
+
+    return pixels;
+}
 
 getVideo();
 video.addEventListener('canplay', paintToCanvas);
