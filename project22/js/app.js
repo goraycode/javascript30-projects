8836db96fa0c0d8f0e423b31b4cbe5a517db3fcf
AYGJ <jofre1236@gmail.com>
2022-06-22 18:09:55 -0500

    Add new function to links

---
+++ project22/js/app.js
@@ -0,0 +1,23 @@
+const triggers = document.querySelectorAll('a');
+const highLight = document.querySelector('.highlight');
+
+function highlighLink() {
+    const linkCoords = this.getBoundingClientRect();
+    console.log(linkCoords);
+    const coords = {
+        width: linkCoords.width,
+        height: linkCoords.height,
+        top: linkCoords.top + window.scrollY,
+        left: linkCoords.left + window.scrollX,
+    }
+
+    console.log(coords.top);
+    highLight.style.width = `${coords.width}px`;
+    highLight.style.height = `${coords.height}px`;
+    highLight.style.transform = `translate(${coords.left}px , ${coords.top}px)`;
+
+}
+
+triggers.forEach(t => {
+    t.addEventListener('mouseenter', highlighLink);
+});
