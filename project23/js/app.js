ad0890e1bf874eaef07d3c7387d33997ff9dc7ef
AYGJ <jofre1236@gmail.com>
2022-06-23 17:04:29 -0500

    Add function about populateVoices and event voiceschanged

--- project23/js/app.js
+++ project23/js/app.js
@@ -5,3 +5,20 @@ const options = document.querySelector('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
 
+msg.text = document.querySelector('[name="text"]').value;
+
+
+function populateVoices() {
+    const voices = this.getVoices();
+    voices.forEach(voice => {
+        const { name } = voice;
+        const opt = document.createElement('option');
+        opt.value = name;
+        opt.textContent = name;
+        voicesDropdown.appendChild(opt);
+    });
+}
+
+
+//get differents voices
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
