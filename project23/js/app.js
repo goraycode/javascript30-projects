5da8b67659f217cba8cd5da00d3b6d020ae05a5d
AYGJ <jofre1236@gmail.com>
2022-06-23 17:34:32 -0500

    Add styles to messageError

--- project23/js/app.js
+++ project23/js/app.js
@@ -1,15 +1,13 @@
 const msg = new SpeechSynthesisUtterance();
 let voices = [];
 const voicesDropdown = document.querySelector('[name="voice"]');
-const options = document.querySelector('[type="range"], [name="text"]');
+const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
-
-msg.text = document.querySelector('[name="text"]').value;
-
+const form = document.querySelector('.container');
 
 function populateVoices() {
-    const voices = this.getVoices();
+    voices = this.getVoices();
     voices.forEach(voice => {
         const { name } = voice;
         const opt = document.createElement('option');
@@ -19,6 +17,50 @@ function populateVoices() {
     });
 }
 
+function setVoice(e) {
+    const chooseVoice = e.target.value;
+    msg.voice = voices.find(voice => voice.name === chooseVoice);
+}
+
+function speakNow() {
+    const valueText = options[2].value;
+    //validate
+    if (valueText === '' || voicesDropdown.value === '') {
+        messageError('Fill out fields');
+        return;
+    }
+
+    msg.text = valueText;
+
+    console.log(msg)
+    /* speechSynthesis.speak(msg); */
+}
+
+function messageError(message) {
+    const divMessage = document.createElement('div');
+    divMessage.classList.add('error');
+    const pText = document.createElement('p');
+    pText.textContent = message;
+    divMessage.appendChild(pText);
+
+
+    const getDiv = document.querySelector('.error');
+    if (getDiv !== null) {
+        getDiv.remove();
+    }
+
+    form.appendChild(divMessage);
+
+    setTimeout(() => {
+        divMessage.remove();
+    }, 2000);
+
+
+}
+
 
 //get differents voices
 speechSynthesis.addEventListener('voiceschanged', populateVoices);
+
+voicesDropdown.addEventListener('change', setVoice);
+speakButton.addEventListener('click', speakNow);
