e1a6d6a68cdc83213056675924051883935c2ac5
AYGJ <jofre1236@gmail.com>
2022-06-24 17:33:42 -0500

    Add styles for responsive

--- project24/js/app.js
+++ project24/js/app.js
@@ -6,10 +6,8 @@ window.addEventListener('scroll', () => {
     const ubication = nav.getBoundingClientRect();
 
     if (ubication.top === 0) {
-        nav.style.backgroundColor = 'red';
         firstLink.classList.add('show');
     } else {
-        nav.style.backgroundColor = 'black';
         firstLink.classList.remove('show');
     }
 
