7e4d10c0704d3dae7ca22d097dd8256a82b01fe8
AYGJ <jofre1236@gmail.com>
2022-06-27 18:04:14 -0500

    Finished project26/

--- project26/js/app.js
+++ project26/js/app.js
@@ -2,12 +2,21 @@ const background = document.querySelector('.dropdownBackground');
 const triggers = document.querySelectorAll('.top > li');
 const nav = document.querySelector('.top');
 
-
 function handleEnter() {
+    background.style.display = 'block';
     this.children[1].classList.add('showInfo');
+    const position = this.getBoundingClientRect();
+    const navCoords = nav.getBoundingClientRect();
+    const coords = {
+        top: position.top - navCoords.top + 40,
+        left: position.left + navCoords.left
+    }
+    console.log(position, coords.left)
+    background.style.transform = `translate(${coords.left}px , ${coords.top}px)`;
 }
 
 function handleLeave() {
+    background.style.display = 'none';
     this.children[1].classList.remove('showInfo')
 }
 
