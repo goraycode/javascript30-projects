a2c5338923c12e01d6689fcd3c97b41a5ab031ca
AYGJ <jofre1236@gmail.com>
2022-06-28 17:26:24 -0500

    Finished project 27 v1.0.0

--- project27/js/app.js
+++ project27/js/app.js
@@ -1,4 +1,7 @@
 const items = document.querySelector('.items');
+let isDown = false;
+//variables
+let startX, scrollLeft;
 
 let number = 35, divsItem = [];
 
@@ -8,6 +11,7 @@ for (let i = 0; i < number; i++) {
 
 }
 
+//functions
 function generarLetra() {
     let letras = ["a", "b", "c", "d", "e", "f", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
     let numero = (Math.random() * 15).toFixed(0);
@@ -36,4 +40,32 @@ function showCards() {
 
 
 
+//Events
 document.addEventListener('DOMContentLoaded', showCards);
+
+items.addEventListener('mousedown', (e) => {
+    isDown = true;
+    items.classList.add('active');
+    startX = e.pageX - items.offsetLeft;
+    scrollLeft = items.scrollLeft;
+});
+
+items.addEventListener('mouseleave', () => {
+    isDown = false;
+    items.classList.remove('active');
+});
+
+items.addEventListener('mouseup', () => {
+    isDown = false;
+    items.classList.remove('active');
+});
+
+items.addEventListener('mousemove', (e) => {
+    if (!isDown) return; //stop the fn from running
+    e.preventDefault();
+    const x = e.pageX - items.offsetLeft;
+    const walk = x - startX;
+    //if i want show card in 2 to 2, mul for n=2
+    //const walk = (x - startX)*n;
+    items.scrollLeft = scrollLeft - walk;
+});
