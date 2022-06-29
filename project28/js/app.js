d3c8e362e0a453f8887235adf78683e122f4f804
AYGJ <jofre1236@gmail.com>
2022-06-28 19:03:51 -0500

    Add function changeRate

--- project28/js/app.js
+++ project28/js/app.js
@@ -1,2 +1,23 @@
 const video = document.querySelector('.panel__video');
-console.dir(video)
+const speed = document.querySelector('#speed');
+const text = document.querySelector('.panel__text');
+
+function changeRate() {
+    const value = this.value;
+    video.playbackRate = value;
+
+    text.textContent = value;
+    if (value >= 3) {
+
+        text.style.transform = `translateX(${value * 3.5}rem)`;
+    } else {
+        text.style.transform = `translateX(${value * 2.2}rem)`;
+    }
+    setTimeout(() => {
+        text.textContent = '';
+    }, 2000);
+}
+
+
+
+speed.addEventListener('change', changeRate);
