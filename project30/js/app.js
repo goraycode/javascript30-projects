a084230cc66edfdd7f56a29ddfe450739817efaf
AYGJ <jofre1236@gmail.com>
2022-07-01 12:49:32 -0500

    Add level2

--- project30/js/app.js
+++ project30/js/app.js
@@ -1,16 +1,20 @@
+import { showBtn } from "./level2.js";
+
 //variables
-const holes = document.querySelectorAll('.hole');
-const score = document.querySelector('.score');
+export const holes = document.querySelectorAll('.hole');
+export const score = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+export const btnStart = document.querySelector('.btnstart');
 let lastHole;
 let sum = 0;
+let upmole;
 
 
-function randomTime(min, max) {
+export function randomTime(min, max) {
     return Math.round(Math.random() * (max - min) + min);
 }
 
-function randomHole(holes) {
+export function randomHole(holes) {
     const idx = Math.floor(Math.random() * holes.length);
     const hole = holes[idx];
     //not same hole
@@ -37,19 +41,32 @@ function peep() {
 }
 
 
+
 function loadPeed() {
+    btnStart.classList.add('hidden');
     const time = randomTime(1000, 2000);
-    setInterval(() => {
+    upmole = setInterval(() => {
         peep();
     }, time);
 }
+
+
 
 function sumScore(e) {
 
     if (!e.isTrusted) return;
     sum++;
+
     score.textContent = sum;
     this.classList.remove('showmole');
+    if (sum === 5) {
+
+        clearInterval(upmole);
+        sum = 0;
+        showBtn();
+
+
+    }
 }
 
 
@@ -59,3 +76,4 @@ moles.forEach(mole => {
 });
 
 
+btnStart.addEventListener('click', loadPeed);
